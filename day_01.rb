40368c8113beb6367453558ce9c1da229e57fa31
j-seixas <jnuno1997@gmail.com>
2020-12-02 13:53:58 +0000

    puzzle 2 of day 01

--- day_01.rb
+++ day_01.rb
@@ -1,14 +1,35 @@
+values = nil
 File.open('inputs/input_day_01.txt', 'r') do |f|
-  break_it = false
   values = f.readlines.map(&:chomp).map(&:to_i)
-  values.each_with_index do |value, index|
-    values[(index + 1)..-1].each do |v|
-      if value + v == 2020
-        puts "#{value}, #{v} - result: #{v * value}"
+end
+
+return unless values
+
+# puzzle 01
+break_it = false
+values.each_with_index do |value, index|
+  values[(index + 1)..-1].each do |v|
+    if value + v == 2020
+      puts "#{value}, #{v} - result: #{v * value}"
+      break_it = true
+      break
+    end
+  end
+  break if break_it
+end
+
+# puzzle 02
+break_it = false
+values.each_with_index do |value, index|
+  values[(index + 1)..-1].each_with_index do |v, i|
+    values[(i + 1)..-1].each do |j|
+      if value + v + j == 2020
+        puts "#{value}, #{v}, #{j} - result: #{v * value * j}"
         break_it = true
         break
       end
     end
     break if break_it
   end
+  break if break_it
 end
