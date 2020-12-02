8abd4ce93d1ddcc37f4c8fe600358378edb97213
j-seixas <jnuno1997@gmail.com>
2020-12-02 20:11:02 +0000

    add structure to day 01

--- day_01.rb
+++ day_01.rb
@@ -1,35 +1,32 @@
-values = nil
-File.open('inputs/input_day_01.txt', 'r') do |f|
-  values = f.readlines.map(&:chomp).map(&:to_i)
+# frozen_string_literal: true
+
+require './utils'
+
+def input_file
+  get_file('inputs/input_day_01.txt').map(&:to_i)
 end
 
-return unless values
-
-# puzzle 01
-break_it = false
-values.each_with_index do |value, index|
-  values[(index + 1)..-1].each do |v|
-    if value + v == 2020
-      puts "#{value}, #{v} - result: #{v * value}"
-      break_it = true
-      break
+def part_one(values)
+  values.each_with_index do |value, index|
+    values[(index + 1)..-1].each do |v|
+      return [value, v, (v * value)] if value + v == 2020
     end
   end
-  break if break_it
 end
 
-# puzzle 02
-break_it = false
-values.each_with_index do |value, index|
-  values[(index + 1)..-1].each_with_index do |v, i|
-    values[(i + 1)..-1].each do |j|
-      if value + v + j == 2020
-        puts "#{value}, #{v}, #{j} - result: #{v * value * j}"
-        break_it = true
-        break
+def part_two(values)
+  input_file.each_with_index do |value, index|
+    values[(index + 1)..-1].each_with_index do |v, i|
+      values[(i + 1)..-1].each do |j|
+        return [value, v, j, (v * value * j)] if value + v + j == 2020
       end
     end
-    break if break_it
   end
-  break if break_it
 end
+
+values = input_file
+v1, v2, r = part_one(values)
+puts "Day 2 - Part 1 --> #{v1} + #{v2} = 2020, #{v1} * #{v2} = #{r}"
+
+v1, v2, v3, r = part_two(values)
+puts "Day 2 - Part 2 --> #{v1} + #{v2} + #{v3} = 2020, #{v1} * #{v2} * #{v3} = #{r}"
