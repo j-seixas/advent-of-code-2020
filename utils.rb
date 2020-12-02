17dd7d6047713b56ca30432b9ee92f73342ab65c
j-seixas <jnuno1997@gmail.com>
2020-12-02 20:10:42 +0000

    fix filename in get_file

--- utils.rb
+++ utils.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def get_file(filename)
-  File.open('inputs/input_day_02.txt', 'r') do |f|
+  File.open(filename, 'r') do |f|
     content = f.readlines.map(&:chomp)
     puts "Error reading file #{filename}" unless content
     content
