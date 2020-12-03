aaf82c540b1f217d0c8508745413b26aa5b26937
j-seixas <jnuno1997@gmail.com>
2020-12-03 09:49:33 +0000

    remove comments

--- day_03.rb
+++ day_03.rb
@@ -21,18 +21,7 @@ def part_one(input, step, pos = [0, 0])
   total
 end
 
-# def part_two(input)
-#   total = 0
-#   passwords.each do |password|
-#     first = password[:password][password[:min] - 1]
-#     second = password[:password][password[:max] - 1]
-#     total += 1 if (first == password[:word] && second != password[:word]) ||
-#                   (first != password[:word] && second == password[:word])
-#   end
-
-#   total
-# end
 
 input = input_file
 puts "Day 3 - Part 1 --> Trees: #{part_one(input, [3, 1])}"
-# puts "Day 2 - Part 2 --> total: #{part_two(passwords)}"
+# puts "Day 3 - Part 2 --> Trees: #{part_two(input)}"
