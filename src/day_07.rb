a2edca74dd7fc5ab6890869906c833dd7bb36a69
j-seixas <jnuno1997@gmail.com>
2020-12-07 13:34:27 +0000

    day 07 part 1

--- src/day_07.rb
+++ src/day_07.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 require './utils/utils'
+require 'active_support/core_ext/hash'
+require 'set'
 
 def input_file
   input = get_file('inputs/input_day_07.txt')
   input.map do |line|
-    values = line.split(/ bags contain | bags?[., ]*|(\d) /).reject(&:empty?)
+    values = line.split(/ bags contain | bags?[., ]*|no other|(\d) /).reject(&:empty?)
     hash = { name: values[0] }
     values[1..-1].each_slice(2) do |count, name|
       hash[name] = count
@@ -14,6 +16,21 @@ def input_file
   end
 end
 
+def get_set_bags(input, bag_name)
+  bags = input.select { |hash| hash.keys.include?(bag_name) }
+  tmp = bags.map { |b| b[:name] }
+  set = tmp.to_set
+  tmp.each do |v|
+    set.merge(get_set_bags(input, v))
+  end
+
+  set
+end
+
+def part_one(input, bag_name)
+  get_set_bags(input, bag_name).size
+end
+
 bags = input_file
-#puts "Day 7 - Part 1 --> Bag colors: #{part_one(bags, 'shiny gold')}"
+puts "Day 7 - Part 1 --> Bag colors: #{part_one(bags, 'shiny gold')}"
 # puts "Day 7 - Part 2 --> Bags: #{part_two(bags)}"
