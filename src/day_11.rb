285d8cc7d5afd49f24dd9524c8494025471041bc
j-seixas <jnuno1997@gmail.com>
2020-12-11 20:08:52 +0000

    day 11 part 2

--- src/day_11.rb
+++ src/day_11.rb
@@ -6,6 +6,30 @@ def input_file
   get_file('inputs/day_11.txt').map { |line| line.split '' }
 end
 
+def check_value(input, row, column, inc_i, inc_j)
+  loop do
+    row += inc_i
+    column += inc_j
+    break if row >= input.size || row.negative? || column >= input[row].size || column.negative?
+
+    return true if input[row][column] == '#'
+    return false if input[row][column] == 'L'
+  end
+  false
+end
+
+def ocupied_two(input, row, column)
+  count = 0
+  (-1..1).each do |i|
+    (-1..1).each do |j|
+      next if i.zero? && j.zero?
+
+      count += 1 if check_value(input, row, column, i, j)
+    end
+  end
+  count
+end
+
 def ocupied_adjacent(input, i, j)
   count = 0
   (i - 1..i + 1).each do |l|
@@ -20,16 +44,16 @@ def ocupied_adjacent(input, i, j)
   count
 end
 
-def iterate(input)
+def iterate(input, part_two = false)
   change = false
   final = deep_copy(input)
   input.each_with_index do |line, i|
     line.each_with_index do |column, j|
-      ocupied = ocupied_adjacent(input, i, j)
+      ocupied = part_two ? ocupied_two(input, i, j) : ocupied_adjacent(input, i, j)
       if column == 'L' && ocupied.zero?
         final[i][j] = '#'
         change = true
-      elsif column == '#' && ocupied >= 4
+      elsif column == '#' && ((ocupied >= 4 && !part_two) || (ocupied >= 5 && part_two))
         final[i][j] = 'L'
         change = true
       end
@@ -49,6 +73,17 @@ def part_one(input)
   count
 end
 
+def part_two(input)
+  change = true
+  iter_input = input
+  change, iter_input = iterate(iter_input, true) while change
+  count = 0
+  iter_input.each do |v|
+    count += v.count('#')
+  end
+  count
+end
+
 seat_layout = input_file
 puts "Day 11 - Part 1 --> Seats ocupied: #{part_one(seat_layout)}"
-# puts "Day 11 - Part 2 --> Seats ocupied: #{part_two(seat_layout)}"
+puts "Day 11 - Part 2 --> Seats ocupied: #{part_two(seat_layout)}"
