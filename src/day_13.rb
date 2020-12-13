80a270445c5224c48c7fe99c0e150357bc1c3fb5
j-seixas <jnuno1997@gmail.com>
2020-12-13 14:38:54 +0000

    day 13 part 2 not working

--- src/day_13.rb
+++ src/day_13.rb
@@ -24,11 +24,31 @@ def part_one(time, input)
     end
     break if minimum.zero?
   end
-  puts " #{best_bus}, #{minimum}"
   best_bus * minimum
 end
 
+def part_two(input)
+  found = false
+  cleaned = input.each_with_index.map { |v, i| [v, i] unless v == 'x' }.compact
+  iter = cleaned.map(&:first).max
+  index = cleaned.map(&:first).find_index(iter)
+  t = iter
+  until found
+    t += iter
+    puts "#{t}"
+    found = true
+    v = t - index
+    cleaned.each do |bus, i|
+      if (v + i) % bus != 0
+        found = false
+        break
+      end
+    end
+  end
+  t
+end
+
 time, buses = input_file
-# puts " #{time}, #{buses}"
+puts " #{time}, #{buses}"
 puts "Day 13 - Part 1 --> ID x wait: #{part_one(time, buses)}"
-# puts "Day 13 - Part 2 --> Distance: #{part_two(buses)}"
+puts "Day 13 - Part 2 --> Distance: #{part_two(buses)}"
